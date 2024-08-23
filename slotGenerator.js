21238fc89aae394eb9e00bd472a9a17e5f920f55
bit-saver <tydahe@gmail.com>
2024-08-23 08:04:05 -0500

    slot generator update

--- slotGenerator.js
+++ slotGenerator.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
-
-const chunks = [];
+import convertNumber from './convertNumber';
+import templates from './slotTemplates'
 
 const replaceAll = (original, root, words, digits) => {
     let result = original;
@@ -21,89 +21,21 @@ const replaceAll = (original, root, words, digits) => {
     return original;
 };
 
-/**
- * config.schema.json
- **/
-let original = `
-"extraTwo": {
-    "title": "Extra 2",
-    "type": "object",
-    "properties": {
-      "sceneCode": {
-        "title": "Extra 2 (AWS): Scene Code",
-        "type": "string",
-        "description": "Enter a scene code from the logs to create a button named 'Extra 2' (in Eve app).",
-        "condition": {
-          "functionBody": "return (model.lightDevices && model.lightDevices[arrayIndices] && model.lightDevices[arrayIndices].deviceId && model.lightDevices[arrayIndices].deviceId.length === 23 && !['switch'].includes(model.lightDevices[arrayIndices].showAs) && !model.lightDevices[arrayIndices].ignoreDevice);"
-        }
-      },
-      "bleCode": {
-        "title": "Extra 2 (BLE): Scene Code",
-        "type": "string",
-        "description": "Can be (1) left blank, (2) used instead of, or (3) used along with the above AWS code. In the case of (3) please make sure both codes correspond to the same Govee mode.",
-        "condition": {
-          "functionBody": "return (model.lightDevices && model.lightDevices[arrayIndices] && model.lightDevices[arrayIndices].deviceId && model.lightDevices[arrayIndices].deviceId.length === 23 && !['switch'].includes(model.lightDevices[arrayIndices].showAs) && !model.lightDevices[arrayIndices].ignoreDevice);"
-        }
-      },
-      "showAs": {
-        "type": "string",
-        "title": "Extra 2: Show As",
-        "description": "Change this setting to Accessory Switch to show this as an extra switch in the Home app.",
-        "oneOf": [
-          {
-            "title": "Eve Switch (Default)",
-            "enum": [
-              "default"
-            ]
-          },
-          {
-            "title": "Accessory Switch",
-            "enum": [
-              "switch"
-            ]
-          }
-        ],
-        "condition": {
-          "functionBody": "return (model.lightDevices && model.lightDevices[arrayIndices] && model.lightDevices[arrayIndices].deviceId && model.lightDevices[arrayIndices].deviceId.length === 23 && !['switch'].includes(model.lightDevices[arrayIndices].showAs) && !model.lightDevices[arrayIndices].ignoreDevice && model.lightDevices[arrayIndices].extraTwo && (model.lightDevices[arrayIndices].extraTwo.sceneCode || model.lightDevices[arrayIndices].extraTwo.bleCode));"
-        }
-      }
-    }
-  },
-`;
+const convert = (element, start, count, root = 'extra') => {
+  if (Array.isArray(element)) {
+    // instead of returning a string, return an array of strings (or arrays), recursive
+    return element.map((part) => convert(part, num, root));
+  }
+  const result = [];
+  for (let i = start; i < i + count; i++) {
+    const numParts = convertNumber(i);
+    result.push(replaceAll(element, 'extra', numParts, i));
+  }
+  return result;
+};
 
-chunks.push(replaceAll(original, 'extra', ['Thirty', 'One'], 31));
+const result = Object.keys(templates).reduce((acc, key) => {
+  acc[key] = convert(templates[key], 32, 9);
+}, {});
 
-/**
- * utils/constants.js
- */
-
-
-/**
- * utils/customer-chars.js
- */
-original = `
-      extraTwo: 'E964F024-079E-48FF-8F27-9C2605A29F52',
-    this.ExtraTwo = function ExtraTwo() {
-      self.hapChar.call(this, 'Extra 2', self.uuids.extraTwo);
-      this.setProps({
-        format: self.hapChar.Formats.BOOL,
-        perms: [self.hapChar.Perms.READ, self.hapChar.Perms.WRITE, self.hapChar.Perms.NOTIFY],
-      });
-      this.value = this.getDefaultValue();
-    };
-    inherits(this.ExtraTwo, this.hapChar);
-    this.ExtraTwo.UUID = this.uuids.extraTwo;
-}
-`;
-
-chunks.push(replaceAll(original, 'extra', ['Thirty', 'One'], 31));
-
-/** 
- * light.js
- */
-
-/**
- * platform.js
- */
-
-fs.writeFileSync('new_slot.txt', chunks.join('\n\n'))
+console.log(JSON.stringify(result, null, 2));
